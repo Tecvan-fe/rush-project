959890f718c4967b859ab58499bfaad13030ca3e
范文杰 <fanwenjie.fe@bytedance.com>
2022-11-16 22:53:35 +0800

    chore: misc

---
+++ apps/foo/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var bar_1 = require("@rush-project/bar");
+var submodule_1 = require("@rush-project/submodule");
+console.log(bar_1["default"], submodule_1["default"]);
---
+++ apps/foo/src/index.ts
@@ -0,0 +1,4 @@
+import bar from '@rush-project/bar';
+import sub from '@rush-project/submodule'
+
+console.log(bar,sub)
